4854ef3085611363d531e5121b0a70afef3e0d81
nicozini <nicolasmartinzini@outlook.com>
2021-04-07 11:52:57 -0300

    Nico Zini - Modificaciones en index; Agrego bolson productos; Configuro Responsive

--- app.js
+++ app.js
@@ -6,39 +6,51 @@ const port = 3030
 const path = require('path');
 app.use(express.static('public'));
 
-
-//Rutas:
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/index.html'));
-});//Home (index.html)
-
-app.get('/productDetail', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/productDetail.html'));
-});//Detalle del producto (productDetail.html)
-
-app.get('/productCart', (req, res) => {
-    res.sendFile(path.join(__dirname,'./views/productCart.html'));
-});//Carrito de compras (productCart.html)
-
-app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/register.html'));
-});//Formulario de registro (register.html)
-
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/login.html'));
-});//Formulario de login (login.html)
-
-app.get('/template', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/template.html'));
-});//Formulario de login (login.html)
-
-app.get('/template2', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/template2.html'));
-});//Formulario de login (login.html)
-
-
-
 app.listen(port, () => {
     console.log('VerduMarket Run on port '+ port);
 });
+
+
+
+
+// ------ Rutas linkeadas ------ //
+
+//Home (index.html)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+
+//Detalle del producto (productDetail.html)
+app.get('/views/productDetail.html', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/productDetail.html'));
+});
+
+//Formulario de registro (register.html)
+app.get('/views/register.html', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'));
+});
+
+//Formulario de login (login.html)
+app.get('/views/login.html', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+});
+
+//Carrito de compras (productCart.html)
+app.get('/views/productCart.html', (req, res) => {
+    res.sendFile(path.join(__dirname,'./views/productCart.html'));
+});
+
+
+
+
+
+// ------ Rutas PENDIENTES ------ //
+
+//    ¿Template?
+app.get('/template2', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/template2.html'));
+});
+
+
+
+
